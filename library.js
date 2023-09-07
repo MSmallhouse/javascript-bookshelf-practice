98c943c5556b00e7f6ec3bd2c6fb59adb3dda1dd
Matt Smallhouse <smallhou@bc.edu>
2023-09-06 21:55:59 -0400

    refactored to use Library class and Book class

--- library.js
+++ library.js
@@ -1,60 +1,69 @@
-const myLibrary = [];
 const shelves = document.querySelector("table");
 const newBookButton = document.getElementById("newBook");
 const confirmButton = document.getElementById("confirmBtn");
+
 const dialog = document.querySelector("dialog");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const numberOfPages = document.getElementById("numberOfPages");
-const read = document.getElementById("read");
+const formTitle = document.getElementById("title");
+const formAuthor = document.getElementById("author");
+const formNumberOfPages = document.getElementById("numberOfPages");
+const formRead = document.getElementById("read");
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary(Book) {
-    myLibrary.push(Book);
-}
-
-function clearDisplay() {
-    let rows = shelves.rows.length;
-    for (let i=0; i<rows; i++) {
-        shelves.deleteRow(0);
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
-function displayBooks() {
-    clearDisplay();
+class Library {
+    constructor() {
+        this.books = [];
+    }
 
-    for (let i=0; i<myLibrary.length; i++) {
-        let book = myLibrary[i];
-        let row = shelves.insertRow();
-        row.setAttribute("row-index", `${i}`);
+    addBookToLibrary(book) {
+        this.books.push(book);
+    }
 
-        let cell = row.insertCell();
-        cell.textContent = `${book.title} by ${book.author}, ${book.pages} pages`;
+    clearDisplay() {
+        let rows = shelves.rows.length;
+        for (let i=0; i<rows; i++) {
+            shelves.deleteRow(0);
+        }
+    }
 
-        readOption = document.createElement("button");
-        readOption.textContent = "Read";
-        readOption.setAttribute("id", `read-option-button-${i}`);
-        readOption.addEventListener("click", readOptionButtonHandler);
-        row.appendChild(readOption);
+    displayBooks() {
+        this.clearDisplay();
 
-        removeButton = document.createElement("button");
-        removeButton.textContent = "Remove Book";
-        removeButton.setAttribute("id", "remove-button");
-        removeButton.addEventListener("click", removeButtonHandler);
-        row.appendChild(removeButton);
+        for (let i=0; i<this.books.length; i++) {
+            let book = this.books[i];
+            let row = shelves.insertRow();
+            row.setAttribute("row-index", `${i}`);
 
+            let cell = row.insertCell();
+            cell.textContent = `${book.title} by ${book.author}, ${book.pages} pages`;
+
+            let readOption = document.createElement("button");
+            readOption.textContent = "Read";
+            readOption.setAttribute("id", `read-option-button-${i}`);
+            readOption.addEventListener("click", readOptionButtonHandler);
+            row.appendChild(readOption);
+
+            let removeButton = document.createElement("button");
+            removeButton.textContent = "Remove Book";
+            removeButton.setAttribute("id", "remove-button");
+            removeButton.addEventListener("click", removeButtonHandler);
+            row.appendChild(removeButton);
+        }
     }
 }
+
+const myLibrary = new Library();
 
 let readOptionButtonHandler = function() {
     let index = this.id.charAt(this.id.length - 1);
-    let book = myLibrary[index];
+    let book = myLibrary.books[index];
     if (this.textContent === "Read") {
         this.textContent = "Unread";
         book.read = false;
@@ -69,7 +78,7 @@ let removeButtonHandler = function() {
     let row = this.parentElement;
     let rowIndex = row.getAttribute("row-index");
     shelves.deleteRow(rowIndex);
-    myLibrary.splice(rowIndex, 1);
+    myLibrary.books.splice(rowIndex, 1);
     // remove from Library now
 }
 
@@ -77,17 +86,17 @@ newBookButton.addEventListener("click", () => {
     dialog.showModal();
 })
 
-title.addEventListener("change", () => {
+formTitle.addEventListener("change", () => {
     confirmButton.value = newBook;
 })
 
 dialog.addEventListener("close", (e) => {
-    displayBooks(myLibrary);
+    myLibrary.displayBooks();
 })
 
 confirmButton.addEventListener("click", (event) => {
     event.preventDefault();
-    let newBook = new Book(title.value, author.value, numberOfPages.value, read.checked);
-    addBookToLibrary(newBook);
+    let newBook = new Book(formTitle.value, formAuthor.value, formNumberOfPages.value, formRead.checked);
+    myLibrary.addBookToLibrary(newBook);
     dialog.close(newBook);
 })
